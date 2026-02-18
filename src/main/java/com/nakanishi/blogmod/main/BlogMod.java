e659527e9daf1475b3133a7774764e4356a510e4
Nakanishi <14projection@gmail.com>
2026-02-18 12:20:53 +0900

    Add files via upload

---
+++ src/main/java/com/nakanishi/blogmod/main/BlogMod.java
@@ -0,0 +1,20 @@
+package com.nakanishi.blogmod.main;
+
+import com.nakanishi.blogmod.registry.BlogModItems;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@SuppressWarnings("removal")
+@Mod(BlogMod.MOD_ID)
+public class BlogMod {
+
+    public static final String MOD_ID = "blog_mod";
+    public BlogMod(){
+
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        BlogModItems.MOD_ITEMS.register(bus);
+
+    }
+}
