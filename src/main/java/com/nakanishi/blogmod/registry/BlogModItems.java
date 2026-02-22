f5dbdcdde92f76364c8cb034802c0d76430db617
Nakanishi <14projection@gmail.com>
2026-02-23 08:47:58 +0900

    Add files via upload

---
+++ src/main/java/com/nakanishi/blogmod/registry/BlogModItems.java
@@ -0,0 +1,21 @@
+package com.nakanishi.blogmod.registry;
+
+import com.nakanishi.blogmod.main.BlogMod;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemNameBlockItem;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class BlogModItems {
+
+    public static final DeferredRegister<Item> MOD_ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BlogMod.MOD_ID);
+
+    public static final RegistryObject<Item> ENDER_APPLE = MOD_ITEMS.register("ender_apple", ()-> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ENDER_CARROT = MOD_ITEMS.register("ender_carrot", ()-> new ItemNameBlockItem(BlogModBlocks.Mod_Blocks.ENDER_CARROTS.get(), new Item.Properties()));
+
+    public static void register(IEventBus eventBus){MOD_ITEMS.register(eventBus);}
+
+}
