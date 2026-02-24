6ed87a58ae6d166d4d5075ab029d193225fdeb6e
Nakanishi <14projection@gmail.com>
2026-02-24 10:25:04 +0900

    Add files via upload

--- src/main/java/com/nakanishi/blogmod/registry/BlogModItems.java
+++ src/main/java/com/nakanishi/blogmod/registry/BlogModItems.java
@@ -1,6 +1,8 @@
 package com.nakanishi.blogmod.registry;
 
 import com.nakanishi.blogmod.main.BlogMod;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemNameBlockItem;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -12,9 +14,9 @@ public class BlogModItems {
 
     public static final DeferredRegister<Item> MOD_ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BlogMod.MOD_ID);
 
-    public static final RegistryObject<Item> ENDER_APPLE = MOD_ITEMS.register("ender_apple", ()-> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ENDER_APPLE = MOD_ITEMS.register("ender_apple", ()-> new Item(new Item.Properties().food(Foods.APPLE)));
 
-    public static final RegistryObject<Item> ENDER_CARROT = MOD_ITEMS.register("ender_carrot", ()-> new ItemNameBlockItem(BlogModBlocks.Mod_Blocks.ENDER_CARROTS.get(), new Item.Properties()));
+    public static final RegistryObject<Item> ENDER_CARROT = MOD_ITEMS.register("ender_carrot", ()-> new ItemNameBlockItem(BlogModBlocks.Mod_Blocks.ENDER_CARROTS.get(), new Item.Properties().food(Foods.CARROT)));
 
     public static void register(IEventBus eventBus){MOD_ITEMS.register(eventBus);}
 
